ffef9cbc277c47ac251020ba8f81e090e74e0185
codym <codym@DESKTOP-L4FV5CP>
2020-04-16 02:00:03 -0500

    Complete

--- Movies/Movie.cs
+++ Movies/Movie.cs
@@ -14,7 +14,13 @@ namespace Movies
         /// Gets or sets the title of the movie
         /// </summary>
         public string Title { get; set; }
-                
+
+        /// <summary>
+        /// Gets or sets Director of the movie
+        /// </summary>
+        public string Director { get; set; }
+
+
         /// <summary>
         /// Gets or sets the MPAA rating of the movie
         /// </summary>
